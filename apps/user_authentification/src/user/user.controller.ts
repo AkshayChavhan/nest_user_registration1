03a681d63c69d5c303a1b0f2696b387cda4554e7
akshaychavhan <akshay@rtledgers.com>
2023-12-15 14:00:42 +0530

    04_controller and services created - User

--- apps/user_authentification/src/user/user.controller.ts
+++ apps/user_authentification/src/user/user.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller , Get, Post} from '@nestjs/common';
+import { UserService } from './user.service';
+import { User } from './schemas/user.schema';
 
-@Controller('user')
-export class UserController {}
+@Controller('users')
+export class UserController {
+    constructor(private userService :  UserService){}
+
+    @Get()
+    async getAllUsers() : Promise<User[]>{
+        return this.userService.findAll();
+    }
+
+
+    @Post()
+    async createUser(
+        @Body()
+        user
+    ): Promise<User> {
+        return this.userService.create(user)
+    }
+}
--- apps/user_authentification/src/user/user.service.ts
+++ apps/user_authentification/src/user/user.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from './schemas/user.schema';
+import mongoose from 'mongoose';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor(
+        @InjectModel(User.name)
+        private userModel : mongoose.Model<User>
+    ){}
+
+
+    async findAll(): Promise<User[]> {
+        const users = await this.userModel.find();
+        return users;
+    }
+
+    async create(user:User): Promise<User> {
+        const res = await this.userModel.create(user);
+        return res;
+    }
+}
